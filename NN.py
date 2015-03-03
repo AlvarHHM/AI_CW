f6f43d622b58c4c09ddfe05318fb16f85a30d1f5
alvar <mahoihei@gmail.com>
2015-03-03 16:57:36 +0000

    dsf

--- NN.py
+++ NN.py
@@ -1,76 +1,78 @@
 import numpy as np
 import math
+
+
 class NN:
-    def __init__(self,inputLayer,hiddenLayer,outputLayer):
-        self.il = inputLayer
-        self.hl = hiddenLayer
-        self.ol = outputLayer
-
+    def __init__(self, input_layer, hidden_layer, no_example):
+        self.il = input_layer
+        self.hl = hidden_layer
+        #self.ihw = np.random.uniform(-1 * 2 / no_example, 2 / no_example, [self.il + 1, self.hl])
+        #self.how = np.random.uniform(-1 * 2 / no_example, 2 / no_example, [self.hl + 1])
+        self.ihw = np.random.uniform(-0.2, 0.2, [self.il + 1, self.hl])
+        self.how = np.random.uniform(-0.2, 0.2, [self.hl + 1])
         self.alpha = 0.1
-        
 
-    def forward(self,input,target):
+    def forward(self, train_data):
+        hidden_neuron_output = np.zeros(self.hl)
+        for hidden_neuron in range(self.hl):
+            output_sum = 0
+            output_sum += self.ihw[-1, hidden_neuron]
+            for input_neuron in range(self.il):
+                output_sum += train_data[input_neuron] * self.ihw[input_neuron, hidden_neuron]
+            hidden_neuron_output[hidden_neuron] = self.sigmoid(output_sum)
 
+        output_sum = 0
+        output_sum += self.how[-1]
+        for hidden_neuron in range(self.hl):
+            output_sum += hidden_neuron_output[hidden_neuron] * self.how[hidden_neuron]
+        output_neuron_output = self.sigmoid(output_sum)
+
+        return hidden_neuron_output, output_neuron_output
+
+    def backward(self, train_output, target):
+        output_delta = (target - train_output[1]) * self.activation(train_output[1])
+        for i in range(self.hl + 1):
+            self.how[i] += self.alpha * output_delta * train_output[1]
+
+        hidden_delta = np.zeros(self.hl)
         for i in range(self.hl):
-            sum = 0
-            #bias
-            sum = sum + self.whl[0,i]
-            for j in range(1,self.il+1):
-                sum = sum + self.whl[j,i] * input[j-1]
-            self.ho[i] = self.sigmoid(sum)
+            hidden_delta[i] = self.how[i] * output_delta * self.activation(train_output[0][i])
+        for i in range(self.il + 1):
+            for j in range(self.hl):
+                self.ihw[i] += self.alpha * hidden_delta[j] * train_output[0][j]
 
-        for i in range(self.ol):
-            sum = 0
-            #bias
-            sum = sum + self.wol[0,i]
-            for j in range(1,self.hl+1):
-                sum = sum + self.wol[j,i] * self.ho[j-1]
-            self.oo[i] = self.sigmoid(sum)
-
-    def backward(self,input,target):
-        #only work when there is only one output
-        o_delta = (target - self.oo[0]) * self.activation(self.oo[0])
-        for j in range(self.hl+1):
-            self.wol[j, 0] += self.alpha * o_delta * self.oo[0]
-
-        for i in range(self.hl):
-            h_delta = self.wol[i+1,0] *  o_delta * self.activation(self.ho[i])
-            self.whl += self.alpha * h_delta * self.ho[i]
-        
-    def train(self,data,iteration=1000):
-        self.whl = np.random.uniform(-1 * 2/len(data),2/len(data),[self.il+1,self.hl])
-        self.wol = np.random.uniform(-1 * 2/len(data),2/len(data),[self.hl+1,self.ol])
-
-        self.io = np.zeros(len(data))
-        self.ho = np.zeros(self.hl)
-        self.oo = np.zeros(self.ol)
-
+    def train(self, data, iteration=1000):
         for i in range(iteration):
             for datum in data:
-                input = datum[0]
-                target = datum[1]
-                self.forward(input,target)
-                self.backward(input,target)
+                output = self.forward(datum[0])
+                self.backward(output, datum[1][0])
 
-    def activation(self,x):
-        return x * (1-x)
+    def test(self, data):
+        for datum in data:
+            print(datum[0], " -> ", self.forward(datum[0]))
 
-    def sigmoid(self,x):
+    @staticmethod
+    def activation(x):
+        return x * (1 - x)
+
+    @staticmethod
+    def sigmoid(x):
         return 1 / (1 + math.exp(-x))
+
+
 def demo():
     pat = [
-        [[0,0], [0]],
-        [[0,1], [1]],
-        [[1,0], [1]],
-        [[1,1], [0]]
+        [[0, 0], [0]],
+        [[0, 1], [1]],
+        [[1, 0], [1]],
+        [[1, 1], [0]]
     ]
 
-    n = NN(2, 2, 1)
+    n = NN(2, 3, len(pat))
     # train it with some patterns
     n.train(pat)
     # test it
-    #n.test(pat)
-
+    n.test(pat)
 
 
 if __name__ == '__main__':
